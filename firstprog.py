b7005dae7423d61dc5ca01e84eb5ce30a015b74f
whitedevil7401 <138320694+whitedevil7401@users.noreply.github.com>
2024-03-09 21:27:55 +0530

    Add files via upload

---
+++ firstprog.py
@@ -0,0 +1,11 @@
+# Write a programme to input the number and print its cube.
+
+num = float(input("Enter a number :"))
+result=num*num*num
+print (result)
+
+# Write a program to input the number and print its square number.
+
+num = float(input("Enter a number :"))
+result=num**num
+print (result)
